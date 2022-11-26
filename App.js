f96947dc88726d8ae3e338ecc433f8ebbc5c36a8
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-11-26 14:00:55 +0300

    exploring layouts using flexbox

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>My list of goals...</Text>
       </View>
     </View>
@@ -15,5 +15,23 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  appContainer: { padding: 48 },
+  appContainer: { paddingTop: 50, paddingHorizontal: 16, flex: 1 },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "firebrick",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "firebrick",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+    borderRadius: 6,
+  },
+  goalsContainer: { flex: 5 },
 });
